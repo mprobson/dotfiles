41f4c63ec789da56d0a6794a04159c1481c0f3bf
Michael Robson <mprobson@illinois.edu>
2014-10-08 01:31:30 -0500

    Change tabs to 2 spaces instead of 4

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -25,17 +25,17 @@ cd $dir
 echo "done"
 
 replace() {
-    # move any existing dotfiles in homedir to dotfiles_old directory
-    mv ~/.$1 $olddir/
-    echo "Creating symlink to $1 in home directory."
-    # then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-    ln -s $dir/$1 ~/.$1
+  # move any existing dotfiles in homedir to dotfiles_old directory
+  mv ~/.$1 $olddir/
+  echo "Creating symlink to $1 in home directory."
+  # then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+  ln -s $dir/$1 ~/.$1
 }
 
 replace2() {
-    mv ~/.$2/$1 $olddir/.$2/
-    echo "Creating symlink to $1 in ~/.$2 directory."
-    ln -s $dir/$2/$1 ~/.$2/$1
+  mv ~/.$2/$1 $olddir/.$2/
+  echo "Creating symlink to $1 in ~/.$2 directory."
+  ln -s $dir/$2/$1 ~/.$2/$1
 }
 
 echo "Moving any existing dotfiles from ~ to $olddir"
@@ -45,13 +45,13 @@ done
 
 # handle directories by just symlinking files
 for folder in $folders; do
-    echo "Creating backup version of $folder"
-    mkdir -p $olddir/.$folder
-    echo "Moving any existing files from .$folder to $olddir/.$folder"
-    cd $dir/$folder
-    pwd
-    # Use the shell's globbing feature to get filenames, thanks stack overflow!
-    for file2 in *; do
-       replace2 $file2 $folder
-    done
+  echo "Creating backup version of $folder"
+  mkdir -p $olddir/.$folder
+  echo "Moving any existing files from .$folder to $olddir/.$folder"
+  cd $dir/$folder
+  pwd
+  # Use the shell's globbing feature to get filenames, thanks stack overflow!
+  for file2 in *; do
+    replace2 $file2 $folder
+  done
 done
