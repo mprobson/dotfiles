071ac614a9b7f0eeaf99dcd41b91505b5ba2a6d9
Michael Robson <mprobson@illinois.edu>
2014-10-07 21:44:11 -0500

    Copy ssh config as well

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gitconfig vim private"    # list of files/folders to symlink in homedir
+files="bashrc vimrc gitconfig vim private ssh"    # list of files/folders to symlink in homedir
 
 ##########
 
