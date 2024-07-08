9e76362a8bcd6c8e6d57c0a62b7cfeca0ae2273b
Michael Robson <mrobson@smith.edu>
2024-07-08 11:24:14 -0400

    wezterm: Initial commit with color switch config

---
+++ wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Default Dark (base16)'
+  else
+    return 'Default Light (base16)'
+  end
+end
+
+return {
+  color_scheme = scheme_for_appearance(get_appearance()),
+}
