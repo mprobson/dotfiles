e2e7352211e39695666e2f06a90640df7d3607d3
Michael Robson <mrobson@smith.edu>
2024-07-22 12:49:04 -0400

    wezterm: More generic config setup

--- wezterm.lua
+++ wezterm.lua
@@ -18,6 +18,27 @@ function scheme_for_appearance(appearance)
   end
 end
 
-return {
-  color_scheme = scheme_for_appearance(get_appearance()),
-}
+-- from danielcooper
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- official docs
+-- This is where you actually apply your config choices
+
+-- my edits (from WezTerm docs)
+-- light/dark
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+-- and finally, return the configuration to wezterm
+return config
+
+-- References
+-- basics: https://wezfurlong.org/wezterm/config/files.html#quick-start
+-- advanced config: https://github.com/danielcopper/dotfiles/blob/arch/.config/wezterm/wezterm.lua
+-- dynamic colors: https://wezfurlong.org/wezterm/config/lua/wezterm.gui/get_appearance.html
