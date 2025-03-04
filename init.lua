5897d90d33008bd0f92075cecca3b8be3f41b108
Michael Robson <mrobson@smith.edu>
2025-03-04 15:28:26 -0500

    nvim: Initial commit of init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+vim.cmd([[
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+]])
+
+-- vim.o.background = "light"
+-- vim.cmd.colorscheme "base16-default-light"
