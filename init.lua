ef40cf59958bef3e4162e797a29d94eb0474f129
Michael Robson <mrobson@smith.edu>
2025-03-05 10:04:19 -0500

    nvim: Add source for commands

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- https://vi.stackexchange.com/questions/12794/how-to-share-config-between-vim-and-neovim/39957#39957
 vim.cmd([[
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
